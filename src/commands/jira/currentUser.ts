46b73646f65f8baa689d64b3d8b01c7e3b84832e
Nick Rundquist <nrundquist@atlassian.com>
2018-10-25 13:35:50 -0500

    VSCODE-35 Add Issue Explorer
    Fetch assigned and open issues from Jira and display their description
    in the explorer.

--- src/commands/jira/currentUser.ts
+++ src/commands/jira/currentUser.ts
@@ -6,7 +6,7 @@ export async function currentUserJira() {
     let client = await Atl.jirarequest();
 
     if (client) {
-        client.getCurrentUser({}).then(user => {
+        client.getCurrentUser({}).then((user: JIRA.Response<JIRA.Schema.User>) => {
             Logger.debug(`currentUser is: ${user.data.displayName}`);
         });
     }
