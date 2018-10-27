2c9d054a50976760559381c4b2e200c2d01d10e8
Pramod Busam <pbusam@atlassian.com>
2018-10-27 00:13:10 -0500

    VSCODE-39: Filter reviewers from participants and show approval state

--- src/webviews/components/PullRequestPage.tsx
+++ src/webviews/components/PullRequestPage.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import Button from '@atlaskit/button';
 import Page, { Grid, GridColumn } from '@atlaskit/page';
-import AvatarGroup from '@atlaskit/avatar-group';
 import { State } from './App';
+import Reviewers from './Reviewers';
 
 export default class PullRequestPage extends React.Component<State, {}> {
     constructor(props: any) {
@@ -18,25 +18,16 @@ export default class PullRequestPage extends React.Component<State, {}> {
 
     render() {
         const pr = this.props.pr;
-        if (!pr) { return <div>No data.</div>; }
-        const participants = pr.participants!.map(p => { return { name: p.user!.username!, src: p.user!.links!.avatar!.href! }; });
+        if (!pr) { return <div>No data!</div>; }
         return (
-            pr &&
-            < Page >
+            <Page>
                 <Grid>
                     <GridColumn medium={8}>
                         <h2><a href={pr.links!.html!.href}>#{pr.id}</a>  {pr.title}</h2>
                         <Button spacing="compact">{pr.source!.branch!.name}</Button> → <Button spacing="compact">{pr.destination!.branch!.name}</Button>
                     </GridColumn>
                     <GridColumn medium={4}>
-                        <p>
-                            <AvatarGroup
-                                appearance="grid"
-                                data={participants}
-                                maxCount={14}
-                                size="medium"
-                            />
-                        </p>
+                        <Reviewers {...this.props} />
                         <Button onClick={this.alertHandler}>Checkout</Button>
                     </GridColumn>
                     <GridColumn>
@@ -47,7 +38,7 @@ export default class PullRequestPage extends React.Component<State, {}> {
                         <hr />
                     </GridColumn>
                 </Grid>
-            </Page >
+            </Page>
         );
     }
 }
