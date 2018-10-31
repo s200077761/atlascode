a1d80aa2a1fe542f9ffc5747d6a26b0076cea7de
Nick Rundquist <nrundquist@atlassian.com>
2018-10-31 14:53:25 -0500

    VSCODE-51 Add web view for Jira issues

--- src/jira/jiraIssue.ts
+++ src/jira/jiraIssue.ts
@@ -1,46 +1,61 @@
 export class JiraIssue {
-    key: string;
-    description: string;
-    summary: string;
-    jiraURL: string;
-    issueIcon: string;
-    comments: JiraComment[];
 
-    constructor(key: string, description: string, summary: string, jiraURL: string, issueIcon: string, comments: JiraComment[]) {
-        this.key = key;
-        this.description = description;
-        this.summary = summary;
-        this.jiraURL = jiraURL;
-        this.issueIcon = issueIcon;
-        this.comments = comments;
-    }
+    static readonly fields = ["summary", "description", "comment", "issuetype", "status", "created", "reporter", "assignee", "labels", "attachment", "status"];
+
+    constructor(readonly key: string, readonly description: string, readonly summary: string, 
+        readonly status: JiraStatus, readonly reporter: JiraUser, readonly assignee: JiraUser | undefined, 
+        readonly jiraURL: string, readonly issueIcon: string, readonly comments: JiraComment[], 
+        readonly labels: string[], readonly attachments: JIRA.Schema.AttachmentBean[]) {}
 
     static readIssue(issueJson: any): JiraIssue {
         const key = issueJson.key;
         const description = issueJson.fields.description;
         const summary = issueJson.fields.summary;
+        const status = new JiraStatus(issueJson.fields.status.name, issueJson.fields.status.statusCategory.colorName);
         const issuetypeIcon = issueJson.fields.issuetype.iconUrl;
+        const reporter = JiraUser.readUser(issueJson.fields.reporter);
+        const assignee = issueJson.fields.assignee ? JiraUser.readUser(issueJson.fields.assignee) : undefined;
         const comments = issueJson.fields.comment.comments.map((commentJson: any) => {
-          let author = commentJson.author.displayName;
+          let author = JiraUser.readUser(commentJson.author);
           let body = commentJson.body;
           return new JiraComment(author, body);
         });
+        const labels = issueJson.fields.labels;
+        const attachments = issueJson.fields.attachment;
         return new JiraIssue(
-          key,
+          key,  
           description,
           summary,
+          status,
+          reporter,
+          assignee,
           "http://www.example.com/",
           issuetypeIcon,
-          comments
+          comments,
+          labels,
+          attachments
         );
       }
 }
-export class JiraComment {
-    author: string;
-    comment: string;
 
-    constructor(author: string, comment: string) {
-        this.author = author;
-        this.comment = comment;
+export class JiraComment {
+    constructor(readonly author: JiraUser, readonly comment: string) {
+    }
+}
+
+export class JiraUser {
+
+    constructor(readonly displayName: string, readonly accountId: string, readonly avatarUrl: string) {
+    }
+
+    static readUser(userObject: any): JiraUser {
+        return new JiraUser(userObject.displayName,
+            userObject.accountId,
+            userObject.avatarUrls['48x48']);
+    }
+}
+
+export class JiraStatus {
+    constructor(readonly name:string, readonly colorName: string) {
     }
 }
