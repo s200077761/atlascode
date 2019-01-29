ce60996b5998c76f8a31afc9cdc452c523d1f3f5
Nick Rundquist <nrundquist@atlassian.com>
2019-01-29 16:47:17 -0600

    VSCODE-149 Poll for new builds
    * Periodically check for changes in build status.

--- src/bitbucket/pipelines.ts
+++ src/bitbucket/pipelines.ts
@@ -11,10 +11,7 @@ export namespace PipelineApi {
     branchName: string
   ): Promise<Bitbucket.Schema.Pipeline[]> {
       const remotes = PullRequestApi.getBitbucketRemotes(repository);
-      const authInfo = await Container.authManager.getAuthInfo(
-        AuthProvider.BitbucketCloud
-      );
-      const accessToken = authInfo!.access;
+      const accessToken = await getAccessToken();
       return Promise.all(remotes.map(remote => {
         return getListForRemote(remote, branchName, accessToken);
       })).then(arrays => {
@@ -22,32 +19,59 @@ export namespace PipelineApi {
       });
   }
 
+  export async function getRecentActivity(repository: Repository): Promise<Bitbucket.Schema.Pipeline[]> {
+    const remotes = PullRequestApi.getBitbucketRemotes(repository);
+    const accessToken = await getAccessToken();
+    return Promise.all(remotes.map(remote => {
+      return getPipelineResults(remote, accessToken);
+    })).then(arrays => {
+      return [].concat.apply([], arrays);
+    });
+  }
+
+  async function getAccessToken(): Promise<string> {
+    return Container.authManager.getAuthInfo(
+      AuthProvider.BitbucketCloud
+    ).then(authInfo => authInfo!.access);
+  }
+
   async function getListForRemote(
     remote: Remote,
     branchName: string,
     accessToken: string
   ): Promise<Bitbucket.Schema.Pipeline[]> {
+    return getPipelineResults(remote, accessToken, `target.branch=${branchName}`);
+  }
+
+  async function getPipelineResults(
+    remote: Remote,
+    accessToken: string,
+    query?: string
+  ): Promise<Bitbucket.Schema.Pipeline[]> {
     let parsed = GitUrlParse(remote.fetchUrl! || remote.pushUrl!);
     const bbBase = "https://api.bitbucket.org/";
     const pipelinesPath = `2.0/repositories/${parsed.owner}/${parsed.name}/pipelines/`;
-    const targetBranchQuery = `target.branch=${branchName}&sort=-created_on`;
-    return fetch (`${bbBase}${pipelinesPath}?${targetBranchQuery}`, {
-      method: 'GET',
-      headers:{
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`
+    var queryParameters = "sort=-created_on";
+    if (query) {
+      queryParameters = `${query}&sort=-created_on`;
+    }
+    return fetch(`${bbBase}${pipelinesPath}?${queryParameters}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`
       }
     })
     .then(res => res.json())
-    .then((res: Bitbucket.Schema.PaginatedPipelines) => {
-      if (res.values) {
-        return res.values;
-      }
-      return [];
-    })
-    .catch((err: any) => {
-      Logger.error(new Error(`Error getting pipelines ${err}`));
-      return Promise.reject();
-    });
+      .then((res: Bitbucket.Schema.PaginatedPipelines) => {
+        if (res.values) {
+          return res.values;
+        }
+        return [];
+      })
+      .catch((err: any) => {
+        Logger.error(new Error(`Error getting pipelines ${err}`));
+        return Promise.reject();
+      });
   }
 }
--- src/webviews/components/config/PipelinesConfig.tsx
+++ src/webviews/components/config/PipelinesConfig.tsx
@@ -18,16 +18,51 @@ export default class PipelinesConfig extends React.Component<{ configData: Confi
         }
     }
 
+    handleInputChange = (e: any, configKey: string) => {
+        const changes = Object.create(null);
+        changes[configKey] = e.target.value;
+
+        if(this.props.onConfigChange) {
+            this.props.onConfigChange(changes);
+        }
+    }
+
     checklabel = (label: string) => <span className='checkboxLabel'>{label}</span>;
 
     render() {
         return (
+            <div>
             <Checkbox
-                value={"bitbucket.pipelines.explorer.enabled"}
+                value={"bitbucket.pipelines.explorerEnabled"}
                 label={this.checklabel("Enable Bitbucket Pipelines Explorer")}
-                isChecked={this.props.configData.config.bitbucket.pipelines.explorer.enabled}
+                isChecked={this.props.configData.config.bitbucket.pipelines.explorerEnabled}
                 onChange={this.onCheckboxChange}
                 name="pipelines-explorer-enabled" />
+            <Checkbox
+                value={"bitbucket.pipelines.monitorEnabled"}
+                label={this.checklabel("Enable Pipelines Build Status Monitor")}
+                isChecked={this.props.configData.config.bitbucket.pipelines.monitorEnabled}
+                onChange={this.onCheckboxChange}
+                name="pipelines-monitor-enabled" />
+                <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    paddingLeft: '24px',
+                }}
+                >
+                    <div className="refreshInterval">
+                    <span>refresh every: </span>
+                    <input style={{ width: '40px' }}
+                        disabled={!this.props.configData.config.bitbucket.pipelines.explorerEnabled && !this.props.configData.config.bitbucket.pipelines.monitorEnabled}
+                        name="pipelines-refresh-interval"
+                        type="number" min="1"
+                        value={this.props.configData.config.bitbucket.pipelines.refreshInterval}
+                        onChange={(e: any) => this.handleInputChange(e, "bitbucket.pipelines.refreshInterval")} />
+                    <span> minutes</span>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
