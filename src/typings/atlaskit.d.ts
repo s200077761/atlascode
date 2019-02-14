0e0a98ae489b99941fe117a28ed96c4c630a98a7
Jonathan Doklovic <doklovic@atlassian.com>
2019-02-14 14:57:38 -0600

    VSCODE-236: dependency cleanup

--- src/typings/atlaskit.d.ts
+++ src/typings/atlaskit.d.ts
@@ -6,19 +6,13 @@ declare module '@atlaskit/avatar-group';
 declare module '@atlaskit/checkbox';
 declare module '@atlaskit/comment';
 declare module '@atlaskit/banner';
-// declare module '@atlaskit/button';
 declare module '@atlaskit/breadcrumbs';
-// declare module '@atlaskit/dropdown-menu';
 declare module '@atlaskit/lozenge';
-// declare module '@atlaskit/icon';
-// declare module '@atlaskit/logo';
 declare module '@atlaskit/tag';
 declare module '@atlaskit/tag-group';
 declare module '@atlaskit/tooltip';
 declare module '@atlaskit/theme';
 declare module '@atlaskit/size-detector';
-// declare module '@atlaskit/flag';
-// declare module '@atlaskit/feedback-collector';
 declare module '@atlaskit/panel';
 
 /* BEGIN - ONLY USE FOR JQL EDITING */
@@ -28,19 +22,12 @@ declare module '@atlaskit/droplist';
 /* END - ONLY USE FOR JQL EDITING */
 
 declare module '@atlaskit/select';
-// declare module '@atlaskit/status';
 declare module '@atlaskit/spinner';
 declare module '@atlaskit/modal-dialog';
 declare module '@atlaskit/inline-dialog';
-// declare module '@atlaskit/field-text';
-// declare module '@atlaskit/textfield';
-// declare module '@atlaskit/textarea';
 declare module '@atlaskit/section-message';
 declare module '@atlaskit/logo/dist/esm/BitbucketLogo/Icon';
 declare module '@atlaskit/logo/dist/esm/JiraLogo/Icon';
-// declare module '@atlaskit/logo/dist/esm/StrideLogo/Icon';
 declare module '@atlaskit/form';
 declare module '@atlaskit/datetime-picker';
-// declare module '@atlaskit/field-radio-group';
 declare module '@atlaskit/radio';
-// declare module '@atlaskit/user-picker';
