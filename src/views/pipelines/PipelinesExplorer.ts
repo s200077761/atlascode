1c95cdd058f54ea6f8249164a2c51ba65b442750
Nick Rundquist <nrundquist@atlassian.com>
2019-02-21 14:48:09 -0600

    VSCODE-261 Fix treeview visibility calls
    * Move CustomJQL analytic call to the correct place.
    
    * Send analytics when pipeline treeview is made visible

--- src/views/pipelines/PipelinesExplorer.ts
+++ src/views/pipelines/PipelinesExplorer.ts
@@ -1,4 +1,4 @@
-import { Disposable, ConfigurationChangeEvent, window, commands } from "vscode";
+import { Disposable, ConfigurationChangeEvent, window, commands, TreeViewVisibilityChangeEvent } from "vscode";
 import { Time } from '../../util/time';
 import { Container } from "../../container";
 import { configuration } from "../../config/configuration";
@@ -7,18 +7,20 @@ import { setCommandContext, CommandContext, PipelinesTreeViewId } from "../../co
 import { BitbucketContext } from "../../bitbucket/context";
 import { PipelinesMonitor } from "./PipelinesMonitor";
 import { Commands } from "../../commands";
+import { AuthProvider } from "../../atlclients/authInfo";
+import { viewScreenEvent } from "../../analytics";
 
 const defaultRefreshInterval = 5 * Time.MINUTES;
 
 export class PipelinesExplorer extends Disposable {
-    private _disposable:Disposable;
+    private _disposable: Disposable;
     private _timer: any | undefined;
     private _refreshInterval = defaultRefreshInterval;
     private _tree: PipelinesTree | undefined;
     private _monitor: PipelinesMonitor | undefined;
 
     constructor(private _ctx: BitbucketContext) {
-        super (() => this.dispose());
+        super(() => this.dispose());
 
         commands.registerCommand(Commands.RefreshPipelines, this.refresh, this);
 
@@ -35,13 +37,14 @@ export class PipelinesExplorer extends Disposable {
         }
 
         if (initializing || configuration.changed(e, 'bitbucket.pipelines.explorerEnabled')) {
-            if(!Container.config.bitbucket.pipelines.explorerEnabled) {
+            if (!Container.config.bitbucket.pipelines.explorerEnabled) {
                 this._tree = undefined;
             } else {
                 const repos = this._ctx.getAllRepositores();
                 if (repos.length > 0) {
                     this._tree = new PipelinesTree(repos);
-                    window.createTreeView(PipelinesTreeViewId, {treeDataProvider: this._tree!});
+                    const treeView = window.createTreeView(PipelinesTreeViewId, { treeDataProvider: this._tree! });
+                    treeView.onDidChangeVisibility(e => this.onTreeDidChangeVisibility(e));
                 }
             }
             setCommandContext(CommandContext.PipelineExplorer, Container.config.bitbucket.pipelines.explorerEnabled);
@@ -90,6 +93,12 @@ export class PipelinesExplorer extends Disposable {
         if (this._timer) {
             clearInterval(this._timer);
             this._timer = undefined;
+        }
+    }
+
+    async onTreeDidChangeVisibility(event: TreeViewVisibilityChangeEvent) {
+        if (event.visible && await Container.authManager.isAuthenticated(AuthProvider.BitbucketCloud)) {
+            viewScreenEvent(PipelinesTreeViewId).then(e => { Container.analyticsClient.sendScreenEvent(e); });
         }
     }
 }
