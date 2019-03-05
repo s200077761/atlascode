18b5afdf84538ecb47d8cab181296a6017f4f9f4
Pramod Busam <pbusam@atlassian.com>
2019-03-05 10:58:06 -0600

    VSCODE-296: Move default site and project selector to a separate component

--- src/webviews/components/config/JiraExplorer.tsx
+++ src/webviews/components/config/JiraExplorer.tsx
@@ -1,26 +1,11 @@
 import * as React from 'react';
 import { Checkbox } from '@atlaskit/checkbox';
 import { ConfigData, emptyConfigData } from '../../../ipc/configMessaging';
-import { Field, CheckboxField } from '@atlaskit/form';
-import Select, { AsyncSelect, components } from '@atlaskit/select';
+import { CheckboxField } from '@atlaskit/form';
 import { chain } from '../fieldValidators';
-import { WorkingSite } from '../../../config/model';
+import JiraSiteProject from './JiraSiteProject';
 
 type changeObject = { [key: string]: any };
-
-const { Option } = components;
-const IconOption = (props: any) => (
-    <Option {...props}>
-        <div ref={props.innerRef} {...props.innerProps} style={{ display: 'flex', 'align-items': 'center' }}><img src={props.data.avatarUrl} width="24" height="24" /><span style={{ marginLeft: '10px' }}>{props.data.name}</span></div>
-    </Option>
-);
-
-const IconValue = (props: any) => (
-    <components.SingleValue {...props}>
-        <div style={{ display: 'flex', 'align-items': 'center' }}><img src={props.data.avatarUrl} width="16" height="16" /><span style={{ marginLeft: '10px' }}>{props.data.name}</span></div>
-    </components.SingleValue>
-
-);
 
 export default class JiraExplorer extends React.Component<{ configData: ConfigData, isLoading: boolean, loadProjectOptions: (input: string) => Promise<any>, onConfigChange: (changes: changeObject, removes?: string[]) => void }, ConfigData> {
 
@@ -48,36 +33,6 @@ export default class JiraExplorer extends React.Component<{ configData: ConfigDa
 
         if (this.props.onConfigChange) {
             this.props.onConfigChange(changes);
-        }
-    }
-
-    handleSiteChange = (item: WorkingSite) => {
-        if (item) {
-            const changes = Object.create(null);
-            const removes = [];
-
-            removes.push('jira.workingProject');
-            changes['jira.workingSite'] = item;
-
-            if (this.props.onConfigChange) {
-                this.props.onConfigChange(changes, removes);
-            }
-        }
-    }
-
-    handleProjectChange = (item: any) => {
-        if (item) {
-            const changes = Object.create(null);
-            changes['jira.workingProject'] = { id: item.id, name: item.name, key: item.key };
-
-            if (this.props.onConfigChange) {
-                this.props.onConfigChange(changes);
-            }
-        } else {
-            const removes = ['jira.workingProject'];
-            if (this.props.onConfigChange) {
-                this.props.onConfigChange([], removes);
-            }
         }
     }
 
@@ -188,61 +143,8 @@ export default class JiraExplorer extends React.Component<{ configData: ConfigDa
                         disabled={!this.props.configData.config.jira.explorer.enabled} />
                     <span> minutes (setting to 0 disables notification)</span>
                 </div>
-                <hr />
 
-                <div className='ac-flex-space-between'>
-                    <Field label='Default Site'
-                        id='defaultSite'
-                        name='defaultSite'
-                        defaultValue={this.state.config.jira.workingSite}
-                    >
-                        {
-                            (fieldArgs: any) => {
-                                return (
-                                    <Select
-                                        {...fieldArgs.fieldProps}
-                                        className="ac-select-container"
-                                        classNamePrefix="ac-select"
-                                        getOptionLabel={(option: any) => option.name}
-                                        getOptionValue={(option: any) => option.id}
-                                        options={this.state.sites}
-                                        components={{ Option: IconOption, SingleValue: IconValue }}
-                                        onChange={chain(fieldArgs.fieldProps.onChange, this.handleSiteChange)}
-                                    />
-                                );
-                            }
-                        }
-                    </Field>
-                    <div className='ac-hmargin' />
-                    <Field defaultValue={this.state.config.jira.workingProject}
-                        label='Project'
-                        id='project'
-                        name='project'
-                    >
-                        {
-                            (fieldArgs: any) => {
-                                return (
-                                    <AsyncSelect
-                                        {...fieldArgs.fieldProps}
-                                        className="ac-select-container"
-                                        classNamePrefix="ac-select"
-                                        getOptionLabel={(option: any) => {
-                                            return `${option.name} (${option.key})`;
-                                        }}
-                                        getOptionValue={(option: any) => {
-                                            return option.key;
-                                        }}
-                                        onChange={chain(fieldArgs.fieldProps.onChange, this.handleProjectChange)}
-                                        defaultOptions={this.state.projects}
-                                        loadOptions={this.props.loadProjectOptions}
-                                        placeholder="Choose a Project"
-                                        isLoading={this.props.isLoading}
-                                    />
-                                );
-                            }
-                        }
-                    </Field>
-                </div>
+                <JiraSiteProject configData={this.state} isLoading={this.props.isLoading} onConfigChange={this.props.onConfigChange} loadProjectOptions={this.props.loadProjectOptions}/>
 
             </div>
 
