56d20f4f95815f7f5a173ca9c3adce19e410ba51
Nick Rundquist <nrundquist@atlassian.com>
2019-03-07 18:56:37 -0600

    VSCODE-309 Fix multiple analytic events on launch
    Introduce a random delay before checking for upgrade or installation in
    the hopes that this will reduce the likelihood of multiple windows
    performing the check simultaneously.

--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,9 @@ import { extensions, ExtensionContext, commands } from 'vscode';
 import * as semver from 'semver';
 import { activate as activateCodebucket } from './codebucket/command/registerCommands';
 import { installedEvent, upgradedEvent } from './analytics';
+import { Memento } from "vscode";
 
+const AnalyticDelay = 5000;
 
 export async function activate(context: ExtensionContext) {
     const start = process.hrtime();
@@ -44,33 +46,35 @@ export async function activate(context: ExtensionContext) {
         Logger.error(new Error('vscode.git extension not found'));
     }
 
-    runInstallationRoutines(atlascodeVersion, previousVersion);
-    context.globalState.update(GlobalStateVersionKey, atlascodeVersion);
+    showWelcomePage(atlascodeVersion, previousVersion);
+    const delay = Math.floor(Math.random() * Math.floor(AnalyticDelay));
+    setTimeout(() => {
+        sendAnalytics(atlascodeVersion, context.globalState);
+    }, delay);
 
     const duration = process.hrtime(start);
     Logger.debug(`Atlassian for VSCode (v${atlascodeVersion}) activated in ${duration[0] * 1000 + Math.floor(duration[1] / 1000000)} ms`);
 }
 
-async function runInstallationRoutines(version: string, previousVersion: string | undefined) {
+async function showWelcomePage(version: string, previousVersion: string | undefined) {
+    if ((previousVersion === undefined || semver.gt(version, previousVersion)) &&
+        Container.config.showWelcomeOnInstall) {
+        await commands.executeCommand(Commands.ShowWelcomePage);
+    }
+}
+
+async function sendAnalytics(version: string, globalState: Memento) {
+    const previousVersion = globalState.get<string>(GlobalStateVersionKey);
+    globalState.update(GlobalStateVersionKey, version);
 
     if (previousVersion === undefined) {
         installedEvent(version).then(e => { Container.analyticsClient.sendTrackEvent(e); });
-
-        if (Container.config.showWelcomeOnInstall) {
-            await commands.executeCommand(Commands.ShowWelcomePage);
-        }
-
         return;
     }
 
     if (semver.gt(version, previousVersion)) {
         Logger.debug(`Atlassian for VSCode upgraded from v${previousVersion} to v${version}`);
-
         upgradedEvent(version, previousVersion).then(e => { Container.analyticsClient.sendTrackEvent(e); });
-
-        if (Container.config.showWelcomeOnInstall) {
-            await commands.executeCommand(Commands.ShowWelcomePage);
-        }
     }
 }
 
