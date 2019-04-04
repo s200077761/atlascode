118d7b44db4b0354c273efc4da934aeb365a2842
Pramod Busam <pbusam@atlassian.com>
2019-04-04 14:28:04 -0500

    VSCODE-387: Implement uninstall hook

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require( 'path' );
+const path = require('path');
 
 module.exports = env => [
     {
@@ -21,14 +21,43 @@ module.exports = env => [
         resolve: {
             extensions: ['.tsx', '.ts', '.js'],
             alias: {
-                "node-fetch": path.resolve( __dirname, 'node_modules/node-fetch/lib/index.js' ),
-                "handlebars": path.resolve( __dirname, 'node_modules/handlebars/dist/handlebars.js' )
+                "node-fetch": path.resolve(__dirname, 'node_modules/node-fetch/lib/index.js'),
+                "handlebars": path.resolve(__dirname, 'node_modules/handlebars/dist/handlebars.js')
             }
         },
         devtool: env.production ? undefined : 'source-map',
         output: {
             filename: '[name].js',
-            path: path.resolve( __dirname, 'build', 'extension' ),
+            path: path.resolve(__dirname, 'build', 'extension'),
+            libraryTarget: "commonjs",
+            devtoolModuleFilenameTemplate: 'file:///[absolute-resource-path]'
+        },
+        externals: ['vscode']
+    },
+    {
+        name: 'uninstall',
+        mode: env.production ? 'production' : 'development',
+        target: 'node',
+        entry: {
+            extension: './src/uninstall/uninstall.ts'
+        },
+        module: {
+            exprContextCritical: false,
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        devtool: env.production ? undefined : 'source-map',
+        output: {
+            filename: 'uninstall.js',
+            path: path.resolve(__dirname, 'build', 'extension'),
             libraryTarget: "commonjs",
             devtoolModuleFilenameTemplate: 'file:///[absolute-resource-path]'
         },
