8bf445f86293f90949304b7e4871bbbd0dc7bb67
Nick Rundquist <nrundquist@atlassian.com>
2019-04-05 16:38:29 -0500

    VSCODE-382 Analytics requests
    * Simplify creation of analytics events.

--- src/analytics.ts
+++ src/analytics.ts
@@ -29,69 +29,21 @@ class AnalyticsPlatform {
 }
 
 export async function installedEvent(version: string): Promise<TrackEvent> {
-
-    const e = {
-        tenantIdType: null,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'installed',
-            actionSubject: 'atlascode',
-            source: 'vscode',
-            attributes: { machineId: Container.machineId, version: version },
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return trackEvent('installed', 'atlascode', { attributes: { machineId: Container.machineId, version: version } });
 }
 
 export async function upgradedEvent(version: string, previousVersion: string): Promise<TrackEvent> {
-    const e = {
-        tenantIdType: null,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'upgraded',
-            actionSubject: 'atlascode',
-            source: 'vscode',
-            attributes: { machineId: Container.machineId, version: version, previousVersion: previousVersion },
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return trackEvent('upgraded', 'atlascode', { attributes: { machineId: Container.machineId, version: version, previousVersion: previousVersion } });
 }
 
 export async function feedbackEvent(feedback: FeedbackData, source: string): Promise<TrackEvent> {
-    const e = {
-        tenantIdType: null,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'submitted',
-            actionSubject: 'atlascodeFeedback',
-            source: source,
-            attributes: { feedback: feedback.description, feedbackType: feedback.type, canContact: feedback.canBeContacted },
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    const attributes = { feedback: feedback.description, feedbackType: feedback.type, canContact: feedback.canBeContacted };
+    return trackEvent('submitted', 'atlascodeFeedback', { source: source, attributes: attributes });
 }
 
 export async function featureChangeEvent(featureId: string, enabled: boolean): Promise<TrackEvent> {
     let action = enabled ? 'enabled' : 'disabled';
-    const e = {
-        tenantIdType: null,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: action,
-            actionSubject: 'feature',
-            actionSubjectId: featureId,
-            source: 'atlascodeSettings'
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return trackEvent(action, 'feature', { actionSubjectId: featureId, source: 'atlascodeSettings' });
 }
 
 export async function authenticateButtonEvent(source: string): Promise<UIEvent> {
@@ -127,37 +79,11 @@ export async function logoutButtonEvent(source: string): Promise<UIEvent> {
 }
 
 export async function authenticatedEvent(hostProduct: string): Promise<TrackEvent> {
-
-    const e = {
-        tenantIdType: null,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'authenticated',
-            actionSubject: 'atlascode',
-            source: 'vscode',
-            attributes: { machineId: Container.machineId, hostProduct: hostProduct },
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return trackEvent('authenticated', 'atlascode', { attributes: { machineId: Container.machineId, hostProduct: hostProduct } });
 }
 
 export async function loggedOutEvent(hostProduct: string): Promise<TrackEvent> {
-
-    const e = {
-        tenantIdType: null,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'unauthenticated',
-            actionSubject: 'atlascode',
-            source: 'vscode',
-            attributes: { machineId: Container.machineId, hostProduct: hostProduct },
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return trackEvent('unauthenticated', 'atlascode', { attributes: { machineId: Container.machineId, hostProduct: hostProduct } });
 }
 
 export async function viewScreenEvent(screenName: string, tenantId?: string): Promise<ScreenEvent> {
@@ -170,231 +96,63 @@ export async function viewScreenEvent(screenName: string, tenantId?: string): Pr
         }
     };
 
-
     return await tenantOrNull<ScreenEvent>(e, tenantId).then(async (o) => { return anyUserOrAnonymous<ScreenEvent>(o); });
 }
 
 export async function siteSelectedEvent(siteId: string): Promise<TrackEvent> {
-    const e = {
-        tenantIdType: 'cloudId',
-        tenantId: siteId,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'selected',
-            actionSubject: 'defaultJiraSite',
-            actionSubjectId: siteId,
-            source: 'vscode'
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return tenantTrackEvent(siteId, 'selected', 'defaultJiraSite', { actionSubjectId: siteId });
 }
 
 export async function projectSelectedEvent(projectId: string, tenantId: string): Promise<TrackEvent> {
-    const e = {
-        tenantIdType: 'cloudId',
-        tenantId: tenantId,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'selected',
-            actionSubject: 'defaultJiraProject',
-            actionSubjectId: projectId,
-            source: 'vscode'
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return tenantTrackEvent(tenantId, 'selected', 'defaultJiraProject', { actionSubjectId: projectId });
 }
 
 export async function issueCreatedEvent(issueKey: string, tenantId: string): Promise<TrackEvent> {
-    const e = {
-        tenantIdType: 'cloudId',
-        tenantId: tenantId,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'created',
-            actionSubject: 'issue',
-            actionSubjectId: issueKey,
-            source: 'vscode'
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return tenantTrackEvent(tenantId, 'created', 'issue', { actionSubjectId: issueKey });
 }
 
 export async function issueTransitionedEvent(issueKey: string, tenantId: string): Promise<TrackEvent> {
-    const e = {
-        tenantIdType: 'cloudId',
-        tenantId: tenantId,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'transitioned',
-            actionSubject: 'issue',
-            actionSubjectId: issueKey,
-            source: 'vscode'
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return tenantTrackEvent(tenantId, 'transitioned', 'issue', { actionSubjectId: issueKey });
 }
 
 export async function issueUrlCopiedEvent(tenantId: string): Promise<TrackEvent> {
-    const e = {
-        tenantIdType: 'cloudId',
-        tenantId: tenantId,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'copied',
-            actionSubject: 'issueUrl',
-            source: 'vscode'
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return tenantTrackEvent(tenantId, 'copied', 'issueUrl');
 }
 
 export async function issueCommentEvent(tenantId: string): Promise<TrackEvent> {
-    const e = {
-        tenantIdType: 'cloudId',
-        tenantId: tenantId,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'created',
-            actionSubject: 'issueComment',
-            source: 'vscode'
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return tenantTrackEvent(tenantId, 'created', 'issueComment');
 }
 
 export async function issueWorkStartedEvent(tenantId: string): Promise<TrackEvent> {
-    const e = {
-        tenantIdType: 'cloudId',
-        tenantId: tenantId,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'workStarted',
-            actionSubject: 'issue',
-            source: 'vscode'
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return tenantTrackEvent(tenantId, 'workStarted', 'issue');
 }
 
 export async function customJQLCreatedEvent(tenantId: string): Promise<TrackEvent> {
-    const e = {
-        tenantIdType: 'cloudId',
-        tenantId: tenantId,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'created',
-            actionSubject: 'customJql',
-            source: 'atlascodeSettings'
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return tenantTrackEvent(tenantId, 'created', 'customJql', { source: 'atlascodeSettings' });
 }
 
 export async function prCreatedEvent(): Promise<TrackEvent> {
-
-    const e = {
-        tenantIdType: null,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'created',
-            actionSubject: 'pullRequest',
-            source: 'vscode',
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return trackEvent('created', 'pullRequest');
 }
 
 export async function prCommentEvent(): Promise<TrackEvent> {
-    const e = {
-        tenantIdType: null,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'created',
-            actionSubject: 'pullRequestComment',
-            source: 'vscode'
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return trackEvent('created', 'pullRequestComment');
 }
 
 export async function prCheckoutEvent(): Promise<TrackEvent> {
-    const e = {
-        tenantIdType: null,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'checkedOut',
-            actionSubject: 'pullRequestBranch',
-            source: 'pullRequestDetailsScreen'
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return trackEvent('checkedOut', 'pullRequestBranch', { source: 'pullRequestDetailsScreen' });
 }
 
 export async function prApproveEvent(): Promise<TrackEvent> {
-    const e = {
-        tenantIdType: null,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'approved',
-            actionSubject: 'pullRequest',
-            source: 'pullRequestDetailsScreen'
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return trackEvent('approved', 'pullRequest', { source: 'pullRequestDetailsScreen' });
 }
 
 export async function prMergeEvent(): Promise<TrackEvent> {
-    const e = {
-        tenantIdType: null,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'merged',
-            actionSubject: 'pullRequest',
-            source: 'pullRequestDetailsScreen'
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return trackEvent('merged', 'pullRequest', { source: 'pullRequestDetailsScreen' });
 }
 
 export async function prUrlCopiedEvent(): Promise<TrackEvent> {
-    const e = {
-        tenantIdType: null,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'copied',
-            actionSubject: 'pullRequestUrl',
-            source: 'vscode'
-        }
-    };
-
-    return await anyUserOrAnonymous<TrackEvent>(e);
+    return trackEvent('copied', 'pullRequestUrl');
 }
 
 export async function prPaginationEvent(): Promise<UIEvent> {
@@ -436,21 +194,45 @@ export async function bbIssuesPaginationEvent(): Promise<UIEvent> {
 }
 
 export async function pipelineStartEvent(): Promise<TrackEvent> {
+    return trackEvent('start', 'pipeline');
+}
+
+export async function startIssueCreationEvent(source: string): Promise<TrackEvent> {
+    return trackEvent('create', 'jiraIssue', { source: source });
+}
+
+async function tenantTrackEvent(tenentId: string, action: string, actionSubject: string, otherStuff: any = {}): Promise<TrackEvent> {
     const e = {
-        tenantIdType: null,
-        trackEvent: {
-            origin: 'desktop',
-            platform: AnalyticsPlatform.for(process.platform),
-            action: 'start',
-            actionSubject: 'pipeline',
-            source: 'vscode'
-        }
+        tenantIdType: 'cloudId',
+        tenantId: tenentId,
+        trackEvent: event(action, actionSubject, otherStuff)
     };
 
     return await anyUserOrAnonymous<TrackEvent>(e);
 }
 
+async function trackEvent(action: string, actionSubject: string, otherStuff: any = {}): Promise<TrackEvent> {
+    const e = {
+        tenantIdType: null,
+        trackEvent: event(action, actionSubject, otherStuff)
+    };
+
+    return await anyUserOrAnonymous<TrackEvent>(e);
+}
+
+function event(action: string, actionSubject: string, otherStuff: any): any {
+    var event = {
+        origin: 'desktop',
+        platform: AnalyticsPlatform.for(process.platform),
+        action: action,
+        actionSubject: actionSubject,
+        source: 'vscode'
+    };
+    return Object.assign(event, otherStuff);
+}
+
 async function anyUserOrAnonymous<T>(e: Object, hostProduct?: string): Promise<T> {
+    console.log(JSON.stringify(e));
     let userType = 'anonymousId';
     let userId = Container.machineId;
     let authInfo: AuthInfo | undefined = undefined;
