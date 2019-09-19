420f45a618380514d3ba1e7657ca74cc596b4dc8
Jonathan Doklovic <doklovic@atlassian.com>
2019-09-19 12:43:02 -0500

    VSCODE-666: combined notifications

--- src/jira/newIssueMonitor.ts
+++ src/jira/newIssueMonitor.ts
@@ -54,13 +54,15 @@ export class NewIssueMonitor {
         );
       });
 
+      const foundIssues: MinimalIssue[] = [];
+
       let jqlResults = await pSettle<JQLSettleResult>(jqlPromises);
       jqlResults.forEach(result => {
         if (result.isFulfilled) {
           const newIssues = result.value.issues.filter(issue => issue.created! > this._timestamp);
           if (newIssues.length > 0) {
-            this.showNotification(result.value.jqlName, newIssues);
             newIssues.forEach(issue => {
+              foundIssues.push(issue);
               if (issue.created! > this._timestamp) {
                 this._timestamp = issue.created!;
               }
@@ -69,23 +71,32 @@ export class NewIssueMonitor {
         }
       });
 
+      const notifyIssues = foundIssues.reduce((result: MinimalIssue[], item: MinimalIssue) => {
+        if (!result.find(iss => iss.key === item.key && iss.siteDetails.id === item.siteDetails.id)) {
+          return result.concat(item);
+        } else {
+          return result;
+        }
+      }, []);
+
+      this.showNotification(notifyIssues);
     } catch (e) {
       Logger.error(new Error(`Error checking for new issues ${e}`));
     }
   }
 
-  private showNotification(jqlName: string, newIssues: MinimalIssue[]) {
+  private showNotification(newIssues: MinimalIssue[]) {
     const issueNames = newIssues.map(issue => `[${issue.key}] "${issue.summary}"`);
     commands.executeCommand(Commands.RefreshJiraExplorer);
     var message = "";
     if (newIssues.length === 1) {
-      message = `${issueNames[0]} added to ${jqlName}`;
+      message = `${issueNames[0]} added to explorer`;
     }
     else if (newIssues.length <= 3) {
-      message = `${issueNames.slice(0, -1).join(', ')} and ${issueNames.slice(-1)} added to ${jqlName}`;
+      message = `${issueNames.slice(0, -1).join(', ')} and ${issueNames.slice(-1)} added to explorer`;
     }
     else {
-      message = `${issueNames.slice(0, 2).join(', ')} and ${newIssues.length - 2} other new issues added to ${jqlName}.`;
+      message = `${issueNames.slice(0, 2).join(', ')} and ${newIssues.length - 2} other new issues added to explorer`;
     }
     window.showInformationMessage(message, "View Atlassian Explorer")
       .then((selection) => {
