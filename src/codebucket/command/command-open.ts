7429d8b22dd6424cfcebbd101db47a531df5f1ec
Pramod Busam <pbusam@atlassian.com>
2019-10-09 11:29:56 -0500

    VSCODE-752: Refactor codebucket features to use site details
    Site details were being used already, but there were unused references to internal bitbucket hosts config that were deleted as part of this change.

--- src/codebucket/command/command-open.ts
+++ src/codebucket/command/command-open.ts
@@ -4,7 +4,7 @@ export class OpenInBitbucketCommand extends CommandBase {
 
   protected async execute(): Promise<void> {
     const backend = await this.getBackend();
-    const remote = await backend.findRemoteHost();
+    const remote = await backend.findBitbucketSite();
     const rev = await backend.findCurrentRevision();
     const filePath = this.getFilePath(backend.root);
 
