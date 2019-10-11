46d05a09fa15b7f46b568187ae7795d473452152
Jonathan Doklovic <doklovic@atlassian.com>
2019-10-11 15:58:57 -0500

    VSCODE-788: using no-image.svg

--- src/webviews/components/index.tsx
+++ src/webviews/components/index.tsx
@@ -31,7 +31,7 @@ window.addEventListener("error", (ee: ErrorEvent) => {
     const targetEL = ee.target as HTMLElement;
     if (ee && targetEL && targetEL.nodeName === 'IMG') {
         const origianlSrc = targetEL.getAttribute('src');
-        targetEL.setAttribute('src', 'vscode-resource:images/atlassian-icon.svg');
+        targetEL.setAttribute('src', 'vscode-resource:images/no-image.svg');
         targetEL.setAttribute('alt', `Unable to load image: ${origianlSrc}`);
         targetEL.setAttribute('title', `Unable to load image: ${origianlSrc}`);
         targetEL.setAttribute('class', 'ac-broken-img');
