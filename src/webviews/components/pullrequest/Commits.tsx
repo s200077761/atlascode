05115e411aab5cb8207ba9b03b8261c64f6d9b5d
Henrik Raitasola <henrik.raitasola@gmail.com>
2019-12-23 10:27:36 +0200

    Match tab width

--- src/webviews/components/pullrequest/Commits.tsx
+++ src/webviews/components/pullrequest/Commits.tsx
@@ -12,48 +12,48 @@ const style = { fontFamily: 'monospace' };
 type ColumnProps = { ts: string; href: string; hash: string; message: string };
 
 const Hash = (props: ColumnProps) => (
-  <Button appearance="subtle-link" href={props.href}>
-    <span style={style}>{props.hash}</span>
-  </Button>
+    <Button appearance="subtle-link" href={props.href}>
+        <span style={style}>{props.hash}</span>
+    </Button>
 );
 const Message = (props: ColumnProps) => (
-  <Tooltip content={props.message}>
-    <p style={{ display: 'inline' }}>{props.message.trim().split('\n')[0]}</p>
-  </Tooltip>
+    <Tooltip content={props.message}>
+        <p style={{ display: 'inline' }}>{props.message.trim().split('\n')[0]}</p>
+    </Tooltip>
 );
 const Timestamp = (props: ColumnProps) => {
-  const d = new Date(props.ts);
-  const isLessThanWeekAgo = differenceInWeeks(d, new Date()) === 0;
-  return (
-    <Tooltip content={d.toLocaleString()}>
-      <p>{isLessThanWeekAgo ? distanceInWordsToNow(d, { addSuffix: true }) : format(d, 'YYYY-MM-DD')}</p>
-    </Tooltip>
-  );
+    const d = new Date(props.ts);
+    const isLessThanWeekAgo = differenceInWeeks(d, new Date()) === 0;
+    return (
+        <Tooltip content={d.toLocaleString()}>
+            <p>{isLessThanWeekAgo ? distanceInWordsToNow(d, { addSuffix: true }) : format(d, 'YYYY-MM-DD')}</p>
+        </Tooltip>
+    );
 };
 
 export const Commits: React.FunctionComponent<{ commits: Commit[] }> = props => {
-  const commitsData = props.commits.map(commit => {
-    return {
-      hash: commit.hash,
-      message: commit.message,
-      href: commit.url,
-      ts: commit.ts
-    };
-  });
-
-  return (
-    <TableTree
-      columns={[Hash, Message, Timestamp]}
-      columnWidths={['120px', '100%', '180px']}
-      items={commitsData.map(c => {
-        const props: ColumnProps = {
-          hash: c.hash.substring(0, 8),
-          message: c.message,
-          href: c.href,
-          ts: c.ts
+    const commitsData = props.commits.map(commit => {
+        return {
+            hash: commit.hash,
+            message: commit.message,
+            href: commit.url,
+            ts: commit.ts
         };
-        return { id: c.hash, content: props };
-      })}
-    />
-  );
+    });
+
+    return (
+        <TableTree
+            columns={[Hash, Message, Timestamp]}
+            columnWidths={['120px', '100%', '180px']}
+            items={commitsData.map(c => {
+                const props: ColumnProps = {
+                    hash: c.hash.substring(0, 8),
+                    message: c.message,
+                    href: c.href,
+                    ts: c.ts
+                };
+                return { id: c.hash, content: props };
+            })}
+        />
+    );
 };
