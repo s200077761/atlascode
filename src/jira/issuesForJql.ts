5e5a2d2dbe57b626dc7e235b813aa7243c236d16
Nick Rundquist <nrundquist@atlassian.com>
2020-01-06 15:00:02 -0600

    VSCODE-586 Update loop structure

--- src/jira/issuesForJql.ts
+++ src/jira/issuesForJql.ts
@@ -10,17 +10,16 @@ export async function issuesForJQL(jql: string, site: DetailedSiteInfo): Promise
   const epicFieldInfo = await Container.jiraSettingsManager.getEpicFieldsForSite(site);
 
   let index = 0;
+  let total = 0;
   let issues: MinimalIssue<DetailedSiteInfo>[] = [];
-  while (true) {
+  do {
     const res = await client.searchForIssuesUsingJqlGet(jql, fields, MAX_RESULTS, index);
     const searchResults = await readSearchResults(res, site, epicFieldInfo);
     // While Cloud will let us fetch 100 at a time it's possible server instances will be configured
     // with a lower maximum, so update the index to reflect what's actually being returned.
     index += searchResults.issues.length;
     issues = issues.concat(searchResults.issues);
-    if (!Container.config.jira.explorer.fetchAllQueryResults || index >= searchResults.total) {
-      break;
-    }
-  }
+    total = searchResults.total;
+  } while (Container.config.jira.explorer.fetchAllQueryResults && index < total);
   return issues;
 }
