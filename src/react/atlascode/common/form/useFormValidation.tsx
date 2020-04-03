a265b8ab8a172f3bf8e2babe65ce801abf73487e
Jonathan Doklovic <doklovic@atlassian.com>
2020-04-03 14:31:43 -0500

    VSCODE-1054: prgress

--- src/react/atlascode/common/form/useFormValidation.tsx
+++ src/react/atlascode/common/form/useFormValidation.tsx
@@ -1,4 +1,5 @@
-import { useRef } from 'react';
+import { useCallback } from 'react';
+import useConstant from 'use-constant';
 
 type InputElement = HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement;
 
@@ -9,44 +10,58 @@ type FieldDescriptor = {
     inputRef: InputElement;
     error: ValidationResult;
     touched: boolean;
+    validator: ValidateFunc | undefined;
 };
 
 type Fields = {
     [k: string]: FieldDescriptor;
 };
-function isCheckboxOrRadio(element?: InputElement): element is HTMLInputElement {
-    return !!element && (element.type === 'checkbox' || element.type === 'radio');
-}
+// function isCheckboxOrRadio(element?: InputElement): element is HTMLInputElement {
+//     return !!element && (element.type === 'checkbox' || element.type === 'radio');
+// }
+
+const handleChange = (e: Event) => {
+    console.log('changed', event);
+};
 
 export const useFormValidation = () => {
-    const fields = useRef<Fields>({});
-    function register<Element extends InputElement = InputElement>(
-        validate?: ValidateFunc
-    ): (ref: Element | null) => void {
-        return (ref: Element | null) => {
+    const fields = useConstant<Fields>(() => ({}));
+
+    const doRegister = useCallback(
+        (ref: InputElement | null, validate?: ValidateFunc) => {
+            console.log('registering', ref);
             if (ref) {
                 if (!ref.name) {
                     return console.warn('ref is missing name @ when trying to register form validation', ref);
                 }
 
-                fields.current[ref.name] = {
+                fields[ref.name] = {
                     inputRef: ref,
                     error: undefined,
-                    touched: false
+                    touched: false,
+                    validator: validate
                 };
+
+                ref.addEventListener('input', handleChange);
             }
-        };
+        },
+        [fields]
+    );
+    function register<Element extends InputElement = InputElement>(): (ref: Element | null) => void;
+    function register<Element extends InputElement = InputElement>(
+        validate: ValidateFunc
+    ): (ref: Element | null) => void;
+    function register<Element extends InputElement = InputElement>(
+        ref?: Element,
+        validate?: ValidateFunc
+    ): ((ref: Element | null) => void) | void {
+        console.log('calling register');
+        if (ref) {
+            doRegister(ref);
+            return;
+        }
+        return doRegister;
     }
 
-    // const register = useCallback((ref: HTMLInputElement | null) => {
-    //     if (ref) {
-    //         if (!ref.name) {
-    //             return console.warn('ref is missing name @ when trying to register form validation', ref);
-    //         }
-    //         const { type, value, checked } = ref;
-    //         console.log('ref', type, value, checked);
-    //     }
-    // }, []);
-
-    return register;
+    return useCallback(register, []);
 };
