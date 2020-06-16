01d4697ddd3c2c049b4ea0268b1be96bd6fbf451
pbusam <pbusam@atlassian.com>
2020-06-16 20:32:09 +0000

    VSCODE-1165: Fix uninstall.ts build time and output size
    getAgent() call was brining in container.ts as dependency which was only being used during debugging.
    Removing that reduced vsix size from 3.1MB -> 2.1MB

--- src/analytics-node-client/src/client.ts
+++ src/analytics-node-client/src/client.ts
@@ -1,8 +1,4 @@
-'use strict';
-
 import Analytics from 'analytics-node';
-import { Container } from '../../container';
-import { getAgent } from '../../jira/jira-client/providers';
 import { requireValue, validateScreenEvent, validateTrackEvent, validateUIEvent } from './preconditions';
 import {
     AnalyticsClientInit,
@@ -167,12 +163,11 @@ class AnalyticsClient {
             version,
         };
 
-        const agent: any | undefined = Container.isDebugging ? getAgent() : undefined;
         this.analyticsClient = new Analytics(ANALYTICS_WRITE_KEY, {
             flushAt: flushAt || DEFAULT_QUEUE_FLUSH_SIZE,
             flushInterval: flushInterval || DEFAULT_QUEUE_FLUSH_INTERVAL,
             host: baseUrl || getUrlFromEnvironment(env),
-            agent: agent,
+            //agent: Container.isDebugging ? getAgent() : undefined,
         });
 
         this.deviceId = deviceId;
