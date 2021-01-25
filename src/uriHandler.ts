392fb3837d9f4c85c45ef131a1f94105a230e38b
Nick Rundquist <nrundquist@atlassian.com>
2021-01-25 14:25:38 -0600

    VSCODE-1293 Handle Checkout URI
    * Some refactoring to reduce duplicated code.
    * Persist ref name so it can be checked out after extension restarts.

--- src/uriHandler.ts
+++ src/uriHandler.ts
@@ -1,16 +1,13 @@
-import { commands, Disposable, env, QuickPickItem, Uri, UriHandler, window } from 'vscode';
+import { Disposable, env, Uri, UriHandler, window } from 'vscode';
 import { ProductJira } from './atlclients/authInfo';
 import { LoginManager } from './atlclients/loginManager';
-import { bitbucketSiteForRemote, clientForHostname } from './bitbucket/bbUtils';
-import { WorkspaceRepo } from './bitbucket/model';
-import { Commands } from './commands';
+import { CheckoutHelper } from './bitbucket/checkoutHelper';
 import { startWorkOnIssue } from './commands/jira/startWorkOnIssue';
 import { Container } from './container';
 import { fetchMinimalIssue } from './jira/fetchIssue';
 import { AnalyticsApi } from './lib/analyticsApi';
 import { ConfigSection, ConfigSubSection } from './lib/ipc/models/config';
 import { Logger } from './logger';
-import { checkout } from './views/pullrequest/gitActions';
 
 const ExtensionId = 'atlassian.atlascode';
 //const pullRequestUrl = `${env.uriScheme}://${ExtensionId}/openPullRequest`;
@@ -33,11 +30,25 @@ export const ONBOARDING_URL = `${env.uriScheme}://${ExtensionId}/openOnboarding`
  *      -- q: repository URL (use encodeURIComponent to encode the URL)
  *      -- source: source from which the URI e.g. browser
  *      e.g. vscode://atlassian.atlascode/cloneRepository?q=https%3A%2F%2Fbitbucket.org%2Fatlassianlabs%2Fatlascode&source=browser
+ *  - finalizeAuthentication:
+ *  - startWorkOnJiraIssue:
+ *  - checkoutBranch:
+ *      -- cloneUrl: url of the repo from which to clone
+ *      -- sourceCloneUrl: for forked repos, the url of the forked repo
+ *      -- ref: name of branch or tag
+ *      -- refType: branch|tag
+ *      e.g. vscode://atlassian.atlascode/checkoutBranch?cloneUrl=git@bitbucket.org%3Aatlassianlabs%2Fatlascode.git&ref=VSCODE-1293-add-checkout-branch-deep-lin&refType=branch
+ *      e.g. vscode://atlassian.atlascode/checkoutBranch?cloneUrl=git@bitbucket.org%3Aatlassianlabs%2Fatlascode.git&ref=2.7.0&refType=tag
+ *      e.g. vscode://atlassian.atlascode/checkoutBranch?cloneUrl=git@bitbucket.org%3Aatlassianlabs%2Fatlascode.git&sourceCloneUrl=git@bitbucket.org%3Arundquist%2Fatlascode-fork-test.git&ref=VSCODE-1293-bravo&refType=branch
  */
 export class AtlascodeUriHandler implements Disposable, UriHandler {
     private disposables: Disposable;
 
-    constructor(private loginManager: LoginManager, private analyticsApi: AnalyticsApi) {
+    constructor(
+        private loginManager: LoginManager,
+        private analyticsApi: AnalyticsApi,
+        private bitbucketHelper: CheckoutHelper
+    ) {
         this.disposables = window.registerUriHandler(this);
     }
 
@@ -56,74 +67,6 @@ export class AtlascodeUriHandler implements Disposable, UriHandler {
             await this.handleStartWorkOnJiraIssue(uri);
         } else if (uri.path.endsWith('checkoutBranch')) {
             await this.handleCheckoutBranch(uri);
-        }
-    }
-
-    private async handleCheckoutBranch(uri: Uri) {
-        try {
-            const query = new URLSearchParams(uri.query);
-            const cloneUrl = decodeURIComponent(query.get('cloneUrl') || '');
-            const sourceCloneUrl = decodeURIComponent(query.get('sourceCloneUrl') || ''); //For branches originating from a forked repo
-            const ref = query.get('ref');
-            const refType = query.get('refType');
-
-            if (!ref || !cloneUrl || !refType) {
-                throw new Error(`Query params are missing data: ${query}`);
-            }
-
-            let wsRepo = this.findRepoInCurrentWorkspace(cloneUrl);
-            if (!wsRepo) {
-                window
-                    .showInformationMessage(
-                        `To checkout ref ${ref}: this repository must be cloned in this workspace`,
-                        'Clone Repo'
-                    )
-                    .then(async (userChoice) => {
-                        if (userChoice === 'Clone Repo') {
-                            await this.showCloneOptions(cloneUrl);
-                        }
-                    });
-
-                //try to find the repo again after cloning
-                wsRepo = this.findRepoInCurrentWorkspace(cloneUrl);
-                if (!wsRepo) {
-                    window
-                        .showInformationMessage(
-                            `Could not find repo in current workspace after attempting to clone. Are you authenticated with Bitbucket?`,
-                            'Open auth settings'
-                        )
-                        .then((userChoice) => {
-                            if (userChoice === 'Open auth settings') {
-                                Container.settingsWebviewFactory.createOrShow({
-                                    section: ConfigSection.Jira,
-                                    subSection: ConfigSubSection.Auth,
-                                });
-                            }
-                        });
-                    return;
-                }
-            }
-
-            if (!wsRepo.mainSiteRemote.site) {
-                //I think this means it's not a Bitbucket repo
-                throw new Error(
-                    `Could not tie ${wsRepo.mainSiteRemote.remote.name} to a BitbucketSite object. Is this a Bitbucket repo?`
-                );
-            }
-
-            //Checkout error-handling/messaging is already handled in checkout() function
-            await checkout(wsRepo, ref, sourceCloneUrl);
-
-            this.analyticsApi.fireDeepLinkEvent(decodeURIComponent(query.get('source') || 'unknown'), 'checkoutBranch');
-
-            if (refType === 'branch') {
-                window.showInformationMessage(`Branch ${ref} successfully checked out`);
-            } else {
-                window.showInformationMessage(`${ref} successfully checked out`);
-            }
-        } catch (e) {
-            Logger.debug('error checkout out branch:', e);
-            window.showErrorMessage('Error checkout out branch (check log for details)');
         }
     }
 
@@ -176,6 +119,31 @@ export class AtlascodeUriHandler implements Disposable, UriHandler {
         }
     }
 
+    private async handleCheckoutBranch(uri: Uri) {
+        const query = new URLSearchParams(uri.query);
+        const cloneUrl = decodeURIComponent(query.get('cloneUrl') || '');
+        const sourceCloneUrl = decodeURIComponent(query.get('sourceCloneUrl') || ''); //For branches originating from a forked repo
+        const ref = query.get('ref');
+        const refType = query.get('refType');
+        if (!ref || !cloneUrl || !refType) {
+            throw new Error(`Query params are missing data: ${query}`);
+        }
+
+        try {
+            const success = await this.bitbucketHelper.checkoutRef(cloneUrl, ref, refType, sourceCloneUrl);
+
+            if (success) {
+                this.analyticsApi.fireDeepLinkEvent(
+                    decodeURIComponent(query.get('source') || 'unknown'),
+                    'checkoutBranch'
+                );
+            }
+        } catch (e) {
+            Logger.debug('error checkout out branch:', e);
+            window.showErrorMessage('Error checkout out branch (check log for details)');
+        }
+    }
+
     private async handlePullRequestUri(uri: Uri) {
         try {
             const query = new URLSearchParams(uri.query);
@@ -184,23 +152,10 @@ export class AtlascodeUriHandler implements Disposable, UriHandler {
                 throw new Error(`Cannot parse pull request URL from: ${query}`);
             }
             const repoUrl = prUrl.slice(0, prUrl.indexOf('/pull-requests'));
-            const site = bitbucketSiteForRemote({
-                name: '',
-                fetchUrl: repoUrl,
-                isReadOnly: true,
-            })!;
-
             const prUrlPath = Uri.parse(prUrl).path;
             const prId = prUrlPath.slice(prUrlPath.lastIndexOf('/') + 1);
 
-            try {
-                const client = await clientForHostname('bitbucket.org');
-                const pr = await client.pullrequests.getById(site, parseInt(prId));
-                const wsRepo = this.findRepoInCurrentWorkspace(repoUrl);
-                Container.pullRequestDetailsWebviewFactory.createOrShow(pr.data.url, { ...pr, workspaceRepo: wsRepo });
-            } catch {
-                this.showLoginMessage();
-            }
+            await this.bitbucketHelper.pullRequest(repoUrl, parseInt(prId));
             this.analyticsApi.fireDeepLinkEvent(decodeURIComponent(query.get('source') || 'unknown'), 'pullRequest');
         } catch (e) {
             Logger.debug('error opening pull request:', e);
@@ -208,39 +163,15 @@ export class AtlascodeUriHandler implements Disposable, UriHandler {
         }
     }
 
-    private showLoginMessage() {
-        window
-            .showInformationMessage(
-                'Cannot open pull request. Authenticate with Bitbucket in the extension settings and try again.',
-                'Open settings'
-            )
-            .then((userChoice) => {
-                if (userChoice === 'Open settings') {
-                    Container.settingsWebviewFactory.createOrShow({
-                        section: ConfigSection.Bitbucket,
-                        subSection: ConfigSubSection.Auth,
-                    });
-                }
-            });
-    }
-
     private async handleCloneRepository(uri: Uri) {
+        const query = new URLSearchParams(uri.query);
+        const repoUrl = decodeURIComponent(query.get('q') || '');
+        if (!repoUrl) {
+            throw new Error(`Cannot parse clone URL from: ${query}`);
+        }
+
         try {
-            const query = new URLSearchParams(uri.query);
-            const repoUrl = decodeURIComponent(query.get('q') || '');
-            if (!repoUrl) {
-                throw new Error(`Cannot parse clone URL from: ${query}`);
-            }
-
-            const wsRepo = this.findRepoInCurrentWorkspace(repoUrl);
-            if (wsRepo !== undefined) {
-                window.showInformationMessage(
-                    `Skipped cloning. Repository is open in this workspace already: ${wsRepo.rootUri}`
-                );
-            } else {
-                this.showCloneOptions(repoUrl);
-            }
-
+            await this.bitbucketHelper.cloneRepository(repoUrl);
             this.analyticsApi.fireDeepLinkEvent(
                 decodeURIComponent(query.get('source') || 'unknown'),
                 'cloneRepository'
@@ -249,33 +180,6 @@ export class AtlascodeUriHandler implements Disposable, UriHandler {
             Logger.debug('error cloning repository:', e);
             window.showErrorMessage('Error cloning repository (check log for details)');
         }
-    }
-
-    private findRepoInCurrentWorkspace(repoUrl: string): WorkspaceRepo | undefined {
-        return Container.bitbucketContext.getBitbucketCloudRepositories().find((wsRepo) => {
-            const site = wsRepo.mainSiteRemote.site!;
-            const fullName = `${site.ownerSlug}/${site.repoSlug}`;
-            return repoUrl.includes(fullName);
-        });
-    }
-
-    private async showCloneOptions(repoUrl: string) {
-        const options: (QuickPickItem & { action: () => void })[] = [
-            {
-                label: 'Clone a new copy',
-                action: () => commands.executeCommand(Commands.CloneRepository, 'uriHandler', repoUrl),
-            },
-            {
-                label: 'Add an existing folder to this workspace',
-                action: () => commands.executeCommand(Commands.WorkbenchOpenRepository, 'uriHandler'),
-            },
-            {
-                label: 'Open repository in an different workspace',
-                action: () => commands.executeCommand(Commands.WorkbenchOpenWorkspace, 'uriHandler'),
-            },
-        ];
-
-        await window.showQuickPick(options).then((selection) => selection?.action());
     }
 
     private async finalizeAuthentication(uri: Uri) {
--- src/views/pullrequest/gitActions.ts
+++ src/views/pullrequest/gitActions.ts
@@ -3,62 +3,11 @@ import { parseGitUrl, urlForRemote } from '../../bitbucket/bbUtils';
 import { PullRequest, WorkspaceRepo } from '../../bitbucket/model';
 import { Container } from '../../container';
 import { Logger } from '../../logger';
+import { Remote, Repository } from '../../typings/git';
 
-//TODO: There are two different checkout functions and addRemote functions... this is unideal. They should be unified, but it's difficult for me
-//to figure out how to do this because of some git nuances
 export async function checkout(wsRepo: WorkspaceRepo, ref: string, forkCloneUrl: string): Promise<boolean> {
     await addSourceRemoteIfNeeded(wsRepo.rootUri, ref, forkCloneUrl);
-
-    try {
-        const scm = Container.bitbucketContext.getRepositoryScm(wsRepo.rootUri)!;
-        await scm.checkout(ref);
-        if (scm.state.HEAD?.behind) {
-            scm.pull();
-        }
-        return true;
-    } catch (e) {
-        if (e.stderr.includes('Your local changes to the following files would be overwritten by checkout')) {
-            return window
-                .showInformationMessage(
-                    `Checkout Failed: You have uncommitted changes`,
-                    'Stash changes and try again',
-                    'Dismiss'
-                )
-                .then(async (userChoice) => {
-                    if (userChoice === 'Stash changes and try again') {
-                        await commands.executeCommand('git.stash');
-                        return await checkout(wsRepo, ref, forkCloneUrl);
-                    } else {
-                        return false;
-                    }
-                });
-        } else {
-            window.showInformationMessage(`${e.stderr}`, `Dismiss`);
-            return false;
-        }
-    }
-}
-
-export async function addSourceRemoteIfNeeded(rootUri: string, ref: string, forkCloneUrl: string) {
-    if (!forkCloneUrl) {
-        return;
-    }
-
-    const scm = Container.bitbucketContext.getRepositoryScm(rootUri)!;
-
-    const parsed = parseGitUrl(forkCloneUrl);
-    await scm
-        .getConfig(`remote.${parsed.name}.url`)
-        .then(async (url) => {
-            if (!url) {
-                await scm.addRemote(parsed.name, forkCloneUrl);
-            }
-        })
-        .catch(async (_) => {
-            await scm.addRemote(parsed.name, forkCloneUrl);
-        });
-
-    await scm.fetch(parsed.name, ref);
+    return checkoutRemote(wsRepo.rootUri, ref);
 }
 
 export async function checkoutPRBranch(pr: PullRequest, branch: string): Promise<boolean> {
@@ -69,10 +18,36 @@ export async function checkoutPRBranch(pr: PullRequest, branch: string): Promise
     }
 
     await addSourceRemoteIfNeededForPR(pr);
+    return checkoutRemote(pr.workspaceRepo.rootUri, branch);
+}
 
-    try {
+// Add source remote (if necessary) if pull request is from a fork repository
+export async function addSourceRemoteIfNeededForPR(pr: PullRequest) {
+    const sourceRemote = sourceRemoteForPullRequest(pr);
+
+    if (sourceRemote && pr.workspaceRepo) {
         const scm = Container.bitbucketContext.getRepositoryScm(pr.workspaceRepo.rootUri)!;
-        await scm.checkout(branch || pr.data.source.branchName);
+
+        await addSourceRemote(scm, sourceRemote.name, sourceRemote.fetchUrl!, pr.data.source.branchName);
+    }
+}
+
+async function addSourceRemoteIfNeeded(rootUri: string, ref: string, forkCloneUrl: string) {
+    if (!forkCloneUrl) {
+        return;
+    }
+
+    const scm = Container.bitbucketContext.getRepositoryScm(rootUri)!;
+
+    const parsed = parseGitUrl(forkCloneUrl);
+    await addSourceRemote(scm, parsed.name, forkCloneUrl, ref);
+}
+
+async function checkoutRemote(rootUri: string, remote: string): Promise<boolean> {
+    const scm = Container.bitbucketContext.getRepositoryScm(rootUri)!;
+    try {
+        await scm.fetch();
+        await scm.checkout(remote);
         if (scm.state.HEAD?.behind) {
             scm.pull();
         }
@@ -88,7 +63,7 @@ export async function checkoutPRBranch(pr: PullRequest, branch: string): Promise
                 .then(async (userChoice) => {
                     if (userChoice === 'Stash changes and try again') {
                         await commands.executeCommand('git.stash');
-                        return await checkoutPRBranch(pr, branch);
+                        return await checkoutRemote(rootUri, remote);
                     } else {
                         return false;
                     }
@@ -100,42 +75,43 @@ export async function checkoutPRBranch(pr: PullRequest, branch: string): Promise
     }
 }
 
-// Add source remote (if necessary) if pull request is from a fork repository
-export async function addSourceRemoteIfNeededForPR(pr: PullRequest) {
+async function addSourceRemote(scm: Repository, name: string, fetchUrl: string, ref: string) {
+    await scm
+        .getConfig(`remote.${name}.url`)
+        .then(async (url) => {
+            if (!url) {
+                await scm.addRemote(name, fetchUrl!);
+            }
+        })
+        .catch(async (_) => {
+            await scm.addRemote(name, fetchUrl!);
+        });
+
+    await scm.fetch(name, ref);
+}
+
+function sourceRemoteForPullRequest(pr: PullRequest): Remote | undefined {
     if (!pr.workspaceRepo) {
-        return;
+        return undefined;
     }
 
-    const scm = Container.bitbucketContext.getRepositoryScm(pr.workspaceRepo.rootUri)!;
-
-    if (pr.data.source.repo.url !== '' && pr.data.source.repo.url !== pr.data.destination.repo.url) {
-        // Build the fork repo remote url based on the following:
-        // 1) The source repo url from REST API returns http URLs, and we want to use SSH protocol if the existing remotes use SSH
-        // 2) We build the source remote git url from the existing remote as the SSH url may be different from http url
-        const parsed = parseGitUrl(urlForRemote(pr.workspaceRepo.mainSiteRemote.remote));
-        const parsedSourceRemoteUrl = parseGitUrl(pr.data.source.repo.url);
-        parsed.owner = parsedSourceRemoteUrl.owner;
-        parsed.name = parsedSourceRemoteUrl.name;
-        parsed.full_name = parsedSourceRemoteUrl.full_name;
-        const sourceRemote = {
-            fetchUrl: parsed.toString(parsed.protocol),
-            // Bitbucket Server personal repositories are of the format `~username`
-            // and `~` is an invalid character for git remotes
-            name: pr.data.source.repo.fullName.replace('~', '__').toLowerCase(),
-            isReadOnly: true,
-        };
-
-        await scm
-            .getConfig(`remote.${sourceRemote.name}.url`)
-            .then(async (url) => {
-                if (!url) {
-                    await scm.addRemote(sourceRemote.name, sourceRemote.fetchUrl!);
-                }
-            })
-            .catch(async (_) => {
-                await scm.addRemote(sourceRemote.name, sourceRemote.fetchUrl!);
-            });
-
-        await scm.fetch(sourceRemote.name, pr.data.source.branchName);
+    if (pr.data.source.repo.url === '' || pr.data.source.repo.url === pr.data.destination.repo.url) {
+        return undefined;
     }
+
+    // Build the fork repo remote url based on the following:
+    // 1) The source repo url from REST API returns http URLs, and we want to use SSH protocol if the existing remotes use SSH
+    // 2) We build the source remote git url from the existing remote as the SSH url may be different from http url
+    const parsed = parseGitUrl(urlForRemote(pr.workspaceRepo.mainSiteRemote.remote));
+    const parsedSourceRemoteUrl = parseGitUrl(pr.data.source.repo.url);
+    parsed.owner = parsedSourceRemoteUrl.owner;
+    parsed.name = parsedSourceRemoteUrl.name;
+    parsed.full_name = parsedSourceRemoteUrl.full_name;
+    return {
+        fetchUrl: parsed.toString(parsed.protocol),
+        // Bitbucket Server personal repositories are of the format `~username`
+        // and `~` is an invalid character for git remotes
+        name: pr.data.source.repo.fullName.replace('~', '__').toLowerCase(),
+        isReadOnly: true,
+    };
 }
