bd936e779292e6b9062690f5f261138501f0f9b2
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2021-06-10 11:11:29 -0400

    Allow numbers in project ID regex

--- src/jira/issueKeyParser.ts
+++ src/jira/issueKeyParser.ts
@@ -1,4 +1,4 @@
-export const IssueKeyRegEx = new RegExp(/[a-zA-Z]+-\d+/g);
+export const IssueKeyRegEx = new RegExp(/[a-zA-Z0-9]+-\d+/g);
 
 export function parseJiraIssueKeys(text?: string): string[] {
     if (!text) {
