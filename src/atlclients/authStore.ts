b488312976c9d5968267f1c269b2867f9e17fad2
Nick Rundquist <nrundquist@atlassian.com>
2022-01-13 09:14:12 -0600

    JS-1306 Remove logging statement

--- src/atlclients/authStore.ts
+++ src/atlclients/authStore.ts
@@ -262,9 +262,6 @@ export class CredentialManager implements Disposable {
             info.state = AuthInfoState.Valid;
         }
 
-        Logger.debug(
-            `Reading from keychain\rkey: ${productKey}\rid: ${credentialId}\rinfo: ${JSON.stringify(info, null, '  ')}`
-        );
         return info;
     }
 
