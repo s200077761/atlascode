f19e9c5eeb80df0bd5b65899346ead51998b85cf
Nick Rundquist <nrundquist@atlassian.com>
2022-01-13 09:18:25 -0600

    JS-1306 Updating readme

--- src/react/atlascode/welcome/Welcome.tsx
+++ src/react/atlascode/welcome/Welcome.tsx
@@ -83,6 +83,13 @@ const WelcomePage: React.FunctionComponent = () => {
                                                 </p>
                                             </div>
                                         </section>
+                                        <h4>🎉 What's New in 2.10.3 🎉</h4>
+                                        <section>
+                                            <h4>✨ Bugs Fixed ✨</h4>
+                                            <ul>
+                                                <li>Update to error logging</li>
+                                            </ul>
+                                        </section>
                                         <h4>🎉 What's New in 2.10.2 🎉</h4>
                                         <section>
                                             <h4>✨ Bugs Fixed ✨</h4>
