8ca9722461c8412444998a4998028d81e325e87a
Stan Dzhumaev <sdzhumaev@atlassian.com>
2024-12-13 13:26:22 -0800

    [AXON-37] Implement headless e2e testing

--- e2e/README.md
+++ e2e/README.md
@@ -27,3 +27,16 @@ Commands related to e2e tests:
     # Run the tests without rebuilding the extension
     # Use this one when iterating
     npm run e2e:rerun
+
+### Running E2E Tests Headless
+
+I'm sure we all love looking at the VSCode UI - but sometimes one might want to take a break from that, and run their tests in console only 😉 Unfortunately, [vscode-extension-tester](https://github.com/redhat-developer/vscode-extension-tester) doesn't have built-in support for [headless](https://en.wikipedia.org/wiki/Headless_browser) execution.
+
+However, we can work around that - by running our tests in Docker! We can mount our whole working directory, and run tests in [xvfb](https://en.wikipedia.org/wiki/Xvfb).
+
+In this folder, there's a `Dockerfile` with a rather lightweight image - `vscode`, `xvfb`, their dependencies, and `npm`/`node` - and some scripts to run it. The intended usage is as follows:
+
+* Build the docker image by running `npm run e2e:docker:build`. You only need to do it once
+* Run one of the two commands:
+    - `npm run e2e:docker` - this will do the full cycle of building the extension and setting up tests. You typically want to run this when you've just updated the extension
+    - `npm run e2e:docker:rerun` - a much faster command to rerun the tests against an already prepared setup. Use this when iterating on tests themselves
--- e2e/tests/dummy.test.ts
+++ e2e/tests/dummy.test.ts
@@ -1,28 +1,62 @@
 // import the webdriver and the high level browser wrapper
-import { assert } from 'chai';
-import { before, VSBrowser, WebDriver } from 'vscode-extension-tester';
+import { expect } from 'chai';
+import { before, VSBrowser, WebDriver, ActivityBar, after } from 'vscode-extension-tester';
 import { describe, it } from 'mocha';
 
 // Let's make sure we can import something from the src folder if needed
 import { AuthInfoVersionKey } from '../../src/constants';
 
 // Create a Mocha suite
-describe('My Test Suite', () => {
+describe('Atlassian Extension', async () => {
     let browser: VSBrowser;
     let driver: WebDriver;
 
     console.log('Oh look, a constant from src!');
     console.log('AuthInfoVersionKey:', AuthInfoVersionKey);
 
+    const { log, debug, info, warn } = console;
     // initialize the browser and webdriver
     before(async () => {
+        // Mocking console.log
+        global.console = {
+            ...console,
+            log: () => {},
+            debug: () => {},
+            info: () => {},
+            warn: () => {},
+        };
+
         browser = VSBrowser.instance;
         driver = browser.driver;
+    });
+
+    after(async () => {
+        // reset global.console
+        global.console = {
+            ...console,
+            log,
+            debug,
+            info,
+            warn,
+        };
     });
 
     // test whatever we want using webdriver, here we are just checking the page title
     it('My Test Case', async () => {
         const title = await driver.getTitle();
-        assert.isTrue(title === 'Getting Started' || title === 'Walkthrough: Setup VS Code');
+        expect(title).to.be.oneOf([
+            'Getting Started',
+            'Walkthrough: Setup VS Code',
+            'Getting Started - Visual Studio Code',
+        ]);
+    });
+
+    it('should be installed', async () => {
+        const activityBar = new ActivityBar();
+        const controls = await activityBar.getViewControls();
+        // Get title from every control
+        const titles = await Promise.all(controls.map(async (control) => control.getTitle()));
+
+        expect('Atlassian').to.be.oneOf(titles);
     });
 });
