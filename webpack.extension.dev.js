47e23a6342c4223501123dab20988142a3575572
Marco <5818581+marcomura@users.noreply.github.com>
2025-06-12 16:25:09 -0700

    Use .env file when debugging (#464)

--- webpack.extension.dev.js
+++ webpack.extension.dev.js
@@ -79,7 +79,6 @@ module.exports = [
                 'process.env.ATLASCODE_FF_OVERRIDES': JSON.stringify(process.env.ATLASCODE_FF_OVERRIDES),
                 'process.env.ATLASCODE_EXP_OVERRIDES_BOOL': JSON.stringify(process.env.ATLASCODE_EXP_OVERRIDES_BOOL),
                 'process.env.ATLASCODE_EXP_OVERRIDES_STRING': JSON.stringify(process.env.ATLASCODE_EXP_OVERRIDES_STRING),
-                'process.env.CI': JSON.stringify(process.env.CI),
             }),
         ],
     },
