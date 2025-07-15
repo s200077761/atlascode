474278b6d314d6761ac0f460d44550f90d4a78cb
Migelangel Carrillo <migelangelcarrillo2@gmail.com>
2025-07-15 14:59:27 -0700

    Addressed the bug found in jiraIssueWebview for currentUser reset (#549)
    * Addressed the bug found in jiraIssueWebview for currentUser reset
    
    Looked to see if it affected votes, watchers, or avatarUrl on page but it did not. Now the user is not reset and the url to the image should not disappear.
    
    * Removed comments for code cleaniness in jiraIssueWebview
    
    * Added back comment above update UI functions in jiraIssueWebview

--- src/webviews/jiraIssueWebview.ts
+++ src/webviews/jiraIssueWebview.ts
@@ -127,20 +127,9 @@ export class JiraIssueWebview
                 this._panel.title = `${this._issue.key}`;
             }
 
-            // const currentBranches = Container.bitbucketContext ?
-            //     Container.bitbucketContext.getAllRepositores()
-            //         .filter(repo => repo.state.HEAD && repo.state.HEAD.name)
-            //         .map(repo => repo.state.HEAD!.name!)
-            //     : [];
-
             this._editUIData = editUI as EditIssueData;
 
-            // msg.workInProgress = this._issue.assignee.accountId === this._currentUserId &&
-            //     issue.transitions.find(t => t.isInitial && t.to.id === issue.status.id) === undefined &&
-            //     currentBranches.find(b => b.toLowerCase().indexOf(issue.key.toLowerCase()) !== -1) !== undefined;
-
             this._editUIData.recentPullRequests = [];
-            this._editUIData.currentUser = emptyUser;
 
             const msg = this._editUIData;
 
