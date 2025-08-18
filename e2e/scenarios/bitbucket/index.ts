d32e2fbd54cef1298a5d6cb240a36549f7d15895
mbohoslavskyi <mbohoslavskyi@atlassian.com>
2025-08-18 14:55:11 +0300

    turned off e2e pr creation (#750)

--- e2e/scenarios/bitbucket/index.ts
+++ e2e/scenarios/bitbucket/index.ts
@@ -1,9 +1,9 @@
 import { authFlow } from './authFlow.spec';
 import { connectRepository } from './connectRepository.spec';
-import { createPullRequest } from './createPullRequest.spec';
+// import { createPullRequest } from './createPullRequest.spec';
 
 export const bitbucketScenarios = [
     { name: 'Authenticate with Bitbucket', run: authFlow },
     { name: 'Connect repository', run: connectRepository },
-    { name: 'Create PullRequest', run: createPullRequest },
+    // { name: 'Create PullRequest', run: createPullRequest },
 ];
