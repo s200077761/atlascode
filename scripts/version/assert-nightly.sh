aee4d12aab2d06cbcc46914178e006100778c3ae
Marco <5818581+marcomura@users.noreply.github.com>
2025-08-26 17:29:42 -0700

    Asserts that nightly version does not exist before building it (#827)

--- scripts/version/assert-nightly.sh
+++ scripts/version/assert-nightly.sh
@@ -5,8 +5,15 @@ version=$1
 
 if [[ $version =~ ^[0-9]+\.[0-9]*[13579]\.[0-9]+.*$ ]]; then
     echo "Version $version is a valid pre-release (odd minor version)"
-    exit 0
 else
     echo "Version $version is not a valid pre-release (even minor version or invalid format)"
     exit 1
 fi
+
+tag="v$version-nightly"
+if git rev-parse "$tag" >/dev/null 2>&1; then
+    echo "Version $version already exists!"
+    exit 1
+fi
+
+exit 0
