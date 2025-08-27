d38aac8cc5aa58723c1fb9c10f6ae063572f126e
Marco <5818581+marcomura@users.noreply.github.com>
2025-08-27 10:15:22 -0700

    Hot fix get-latest-nightly (#832)

--- scripts/version/get-latest-nightly.sh
+++ scripts/version/get-latest-nightly.sh
@@ -4,7 +4,7 @@
 # Pre-releases are defined by the version number having an ODD minor number
 # https://code.visualstudio.com/api/working-with-extensions/publishing-extension#prerelease-extensions
 git tag --list | \
-    grep -E '^v[0-9]+\.([1-9][0-9]*)?[13579]\.[0-9]+$' | \
+    grep -E '^v[0-9]+\.([1-9][0-9]*)?[13579]\.[0-9]+' | \
     sort --version-sort -r | \
     head -n 1 | \
     cut -c 2- | \
