405e5423f8175c5e2535b3ccaa1b233e71c1b948
Stan Dzhumaev <sdzhumaev@atlassian.com>
2025-10-02 09:16:32 -0700

    chore: switch to a new feature flag for issue suggestions (#1089)

--- src/util/features.ts
+++ src/util/features.ts
@@ -4,7 +4,7 @@ export enum Features {
     StartWorkV3 = 'atlascode-start-work-v3',
     RovoDevEnabled = 'rovo_dev_ff',
     UseNewAuthFlow = 'atlascode-use-new-auth-flow',
-    EnableAiSuggestions = 'atlascode-enable-ai-suggestions',
+    EnableAiSuggestions = 'atlascode-enable-ai-suggestions-new',
     AtlaskitEditor = 'atlascode-use-new-atlaskit-editor',
 }
 
